f651f3a216146986a2ed5e42e1d527cc82b44b96
noahruppe <noah.ruppe30@gmail.com>
2025-05-29 19:02:27 -0400

    header and login set up

---
+++ handcrafted-haven/src/app/login/page.tsx
@@ -0,0 +1,7 @@
+export default function logIn() {
+  return (
+    <div>
+      <h1>login</h1>
+    </div>
+  );
+}
--- handcrafted-haven/src/app/page.tsx
+++ handcrafted-haven/src/app/page.tsx
@@ -1,10 +1,13 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Header from "./ui/header"; 
+
 
 export default function Home() {
   return (
     <div>
-      <h1>Welcome to the home page</h1>
+      <Header />
+      <main>
+        <p>Welcome to the home page</p>
+      </main>
     </div>
   );
 }
