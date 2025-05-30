f0aa6e777dde1e21f59959a6d4210d0635dea9f2
noahruppe <noah.ruppe30@gmail.com>
2025-05-29 20:44:34 -0400

    header change

--- handcrafted-haven/src/app/ui/header.tsx
+++ handcrafted-haven/src/app/ui/header.tsx
@@ -1,47 +1,20 @@
-"use client";
-
-import Header from "../ui/header";
+import Image from "next/image";
 import Link from "next/link";
-import styles from "./login.module.css"; // your CSS module file
+import styles from "../page.module.css";
 
-export default function LoginPage() {
+export default function Header() {
   return (
-    <div>
-      <Header />
-      <main className={styles.container}>
-        <h2 className={styles.title}>Login to your account</h2>
-        <form action="/api/login" method="POST" className={styles.form}>
-          <label htmlFor="username" className={styles.label}>Username</label>
-          <input
-            type="text"
-            id="username"
-            name="username"
-            required
-            className={styles.input}
-          />
-
-          <label htmlFor="password" className={styles.label}>Password</label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            required
-            className={styles.input}
-          />
-
-          <div className={styles.buttonGroup}>
-            {/* Login is submit button */}
-            <button type="submit" className={styles.button}>
-              Log In
-            </button>
-
-            {/* Sign Up navigates to /signup using Link */}
-            <Link href="/signup" className={styles.linkButton}>
-              Sign Up
-            </Link>
-          </div>
-        </form>
-      </main>
-    </div>
+    <header className={styles.header}>
+      <Image
+        className={styles.image}
+        src="/handcrafted.png"
+        alt="Logo"
+        width={150}
+        height={150}
+      />
+      <Link className={styles.link} href="/login">
+        <button className={styles.button}>Login</button>
+      </Link>
+    </header>
   );
 }
