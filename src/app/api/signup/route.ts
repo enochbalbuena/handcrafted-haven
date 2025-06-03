fe6bd7e8f00d8025c74ffdb25ab2d6c66fcfdd0f
noahruppe <noah.ruppe30@gmail.com>
2025-06-02 22:29:27 -0400

    add signups to the database

---
+++ src/app/api/signup/route.ts
@@ -0,0 +1,41 @@
+import bcrypt from "bcryptjs";
+import { supabase } from "@/lib/database";
+import { error } from "console";
+import { validateUserInput } from "@/lib/validation"
+import { checkUsernameExists } from "@/lib/validation";
+import { json } from "stream/consumers";
+
+export async function POST(req: Request){
+  try{
+    const body = await req.json();
+    const { name, username, password } = body;
+    const errorMessage = validateUserInput(body);
+    const checkUsername = await checkUsernameExists(body.username)
+ 
+    if(errorMessage) return new Response(
+      JSON.stringify({error: errorMessage}), {status: 400}
+    );
+
+    if (checkUsername) return new Response(JSON.stringify({error: checkUsername}), {status:400});
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    const {data, error} = await supabase
+    .from("users")
+    .insert([{name,username,password: hashedPassword}]);
+
+    if (error){
+      return new Response(JSON.stringify({error: `Database error: ${error.message}`}), {status:500});
+    }
+
+    if(!error) return new Response(JSON.stringify({message: "You are logged in"}), {status:201});
+
+
+    
+  }catch (error){
+      return new Response(
+        JSON.stringify({error: "a error occured"}), {status: 500}
+      );
+  }
+}
---
+++ src/lib/validation.ts
@@ -0,0 +1,26 @@
+import { supabase } from "./database";
+
+
+export function validateUserInput(data:{name?: string; username?: string; password?:string;}): string |null {
+    if (!data.name) return "Name is required";
+    if (!data.username) return "Username is required";
+    if (!data.password) return "Password is required";
+    return null;
+}
+
+export async function checkUsernameExists(username: string): Promise<string | null> {
+    const {data, error} = await supabase
+    .from("users")
+    .select("*")
+    .eq("username", username)
+
+    if(error){
+        return `Database error: ${error.message}`;
+    }
+
+    if(data.length > 0){
+        return "Username already exist"
+    }
+
+    return null
+}
